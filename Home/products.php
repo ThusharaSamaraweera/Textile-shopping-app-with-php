1ea13894e541c2ded6eeb8a80f38d1ef9c40773f
Thushara Samaraweera <thusharadasun204@gmail.com>
2021-09-25 20:19:11 +0530

    update

--- Home/products.php
+++ Home/products.php
@@ -21,7 +21,7 @@
 <html>
 
 <head>
-    <link rel="shortcut icon" href="img/logo.png" />
+    <link rel="shortcut icon" href="./Image/logo.png" />
     <title>Kingsmen Products</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
